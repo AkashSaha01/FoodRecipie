8408e43a70f5c2aaaca861ba1562750eec9c2f68
AkashSaha01 <akashsaha661@gmail.com>
2025-06-14 22:50:56 +0600

    Search Bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 function App() {
   return (
     <>
@@ -5,6 +6,7 @@ function App() {
         <div className="w-full py-2 mx-auto">
           <img src="/logo.png" className="mx-auto w-[150px] h-auto py-2" />
         </div>
+
         <div>
           <div
             className="relative h-64 r"
@@ -21,6 +23,20 @@ function App() {
                 flavors of Greek cooking
               </p>
             </div>
+          </div>
+        </div>
+        <div className="w-[75%] mx-auto flex justify-center mt-6">
+          <div className="flex items-center max-w-xl w-full gap-1">
+            <div className="flex-grow border border-gray-300 rounded-md overflow-hidden">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-full px-4 py-3 text-gray-700 focus:outline-none"
+              />
+            </div>
+            <button className="bg-yellow-500 px-4 py-3 rounded-md flex items-center justify-center">
+              <Search className="text-white w-5 h-5" />
+            </button>
           </div>
         </div>
       </div>
